c5daba7bea3b03b7dd16a3982da59df1f6e2639d
etiennecouturier <iistvan.szabo2@gmail.com>
2021-04-02 02:20:09 +0200

    write to file

--- FileViewer/FileOps.h
+++ FileViewer/FileOps.h
@@ -4,6 +4,9 @@
 #include <vector>
 #include <map>
 #include <functional>
+#include <string>
+#include <iostream>
+#include <fstream>
 
 class FileOps
 {
@@ -16,6 +19,9 @@ public:
 
 	template <typename T>
 	static void printFiles(map<T, vector<LocalFile>> groupedFiles);
+
+	template <typename T>
+	static void writeToFiles(map<T, vector<LocalFile>> groupedFiles);
 
 };
 
@@ -34,5 +40,21 @@ static void FileOps::printFiles(map<T, vector<LocalFile>> groupedFiles) {
 			cout << file.getNev() << " " << file.getMeret() << "KB " << file.getDatum() << "\n";
 		}
 		cout << "\n";
+	}
+}
+
+template <typename T>
+static void FileOps::writeToFiles(map<T, vector<LocalFile>> groupedFiles) {
+	int fileId = 1;
+	for (auto group : groupedFiles) {
+		//(string) group.first;
+		ofstream files;
+		files.open("C:\\Temp\\" + to_string(fileId) + ".txt");
+		for (auto file : group.second) {
+			files << file.getNev() << " " << file.getMeret() << "KB " << file.getDatum() << "\n";
+		}
+		files << "\n";
+		fileId++;
+		files.close();
 	}
 }
--- FileViewer/FileViewer.cpp
+++ FileViewer/FileViewer.cpp
@@ -4,20 +4,21 @@ using namespace std;
 
 int main()
 {
-	string folder = "C:\\Users\\istvan\\Downloads\\leggyakoribb\\*";
-	int criteria = 1;
+	string folder = "C:\\Users\\istvan\\Downloads\\*";
+	int criteria;
+	cin >> criteria;
 
 	vector<LocalFile> files = FileOps::get_file_list(folder);
 
 	switch (criteria) {
 	case 1:
-		FileOps::printFiles<string>(FileOps::groupFiles<string>(files, [](LocalFile f) -> string {return f.getNevKiterjesztesNelkul();}));
+		FileOps::writeToFiles<string>(FileOps::groupFiles<string>(files, [](LocalFile f) -> string {return f.getNevKiterjesztesNelkul();}));
 		break;
 	case 2:
-		FileOps::printFiles<int>(FileOps::groupFiles<int>(files, [](LocalFile f) -> int {return f.getMeret();}));
+		FileOps::writeToFiles<int>(FileOps::groupFiles<int>(files, [](LocalFile f) -> int {return f.getMeret();}));
 		break;
 	case 3:
-		FileOps::printFiles<string>(FileOps::groupFiles<string>(files, [](LocalFile f) -> string {return f.getDatum();}));
+		FileOps::writeToFiles<string>(FileOps::groupFiles<string>(files, [](LocalFile f) -> string {return f.getDatum();}));
 		break;
 	default:
 		cout << "Invalid option!";
