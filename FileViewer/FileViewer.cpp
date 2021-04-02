f8f490595c215e8e840aed74c20b897290ecdc6f
etiennecouturier <iistvan.szabo2@gmail.com>
2021-04-02 03:00:44 +0200

    write to file

--- FileViewer/FileViewer.cpp
+++ FileViewer/FileViewer.cpp
@@ -2,6 +2,19 @@
 
 using namespace std;
 
+function<string(LocalFile)> getFunc(int criteria) {
+	switch (criteria) {
+	case 1:
+		return [](LocalFile f) -> string {return f.getNevKiterjesztesNelkul();};
+	case 2:
+		return [](LocalFile f) -> string {return f.getMeretMertekEgyseggel();};
+	case 3:
+		return [](LocalFile f) -> string {return f.getDatum();};
+	default:
+		return [](LocalFile f) -> string {return f.getNevKiterjesztesNelkul();};
+	}
+}
+
 int main()
 {
 	string folder = "C:\\Users\\istvan\\Downloads\\*";
@@ -9,20 +22,5 @@ int main()
 	cin >> criteria;
 
 	vector<LocalFile> files = FileOps::get_file_list(folder);
-
-	switch (criteria) {
-	case 1:
-		FileOps::writeToFiles(FileOps::groupFiles<string>(files, [](LocalFile f) -> string {return f.getNevKiterjesztesNelkul();}));
-		break;
-	case 2:
-		FileOps::writeToFiles(FileOps::groupFiles<string>(files, [](LocalFile f) -> string {return f.getMeretMertekEgyseggel();}));
-		break;
-	case 3:
-		FileOps::writeToFiles(FileOps::groupFiles<string>(files, [](LocalFile f) -> string {return f.getDatum();}));
-		break;
-	default:
-		cout << "Invalid option!";
-	}
-
-	return 0;
+	FileOps::writeToFiles(FileOps::groupFiles<string>(files, getFunc(criteria)));
 }
