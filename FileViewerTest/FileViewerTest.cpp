d3a0bcaaba5d3ec87c91b57dbc27c0f1022f41d8
etiennecouturier <iistvan.szabo2@gmail.com>
2021-04-06 12:51:41 +0200

    refine test

--- FileViewerTest/FileViewerTest.cpp
+++ FileViewerTest/FileViewerTest.cpp
@@ -18,11 +18,11 @@ namespace FileViewerTest
 
 	public:
 		
-		TEST_METHOD(TestMethod1)
+		TEST_METHOD(TestOrderByName)
 		{
 			vector<LocalFile> files;
 			LocalFile file1("ibolya.txt", 3, "2020-03-04");
-			LocalFile file2("ibolya.png", 3, "2020-03-05");
+			LocalFile file2("ibolya.png", 10, "2020-03-05");
 			files.push_back(file1);
 			files.push_back(file2);
 
@@ -30,27 +30,27 @@ namespace FileViewerTest
 			Assert::AreEqual(1, static_cast<int>(result.size()));
 		}
 
-		TEST_METHOD(TestMethod2)
-		{
-			vector<LocalFile> files;
-			LocalFile file1("ibolya.txt", 3, "2020-03-04");
-			LocalFile file2("ibolya.txt", 8, "2020-03-05");
-			files.push_back(file1);
-			files.push_back(file2);
-
-			map<string, vector<LocalFile>> result = FileOps::groupFiles<string>(files, orderByMeret);
-			Assert::AreEqual(2, static_cast<int>(result.size()));
-		}
-
-		TEST_METHOD(TestMethod3)
+		TEST_METHOD(TestOrderByMeret)
 		{
 			vector<LocalFile> files;
 			LocalFile file1("ibolya.txt", 8, "2020-03-04");
-			LocalFile file2("ibolya.txt", 8, "2020-03-05");
+			LocalFile file2("tulipan.txt", 8, "2020-03-05");
 			files.push_back(file1);
 			files.push_back(file2);
 
 			map<string, vector<LocalFile>> result = FileOps::groupFiles<string>(files, orderByMeret);
+			Assert::AreEqual(1, static_cast<int>(result.size()));
+		}
+
+		TEST_METHOD(TestOrderByDate)
+		{
+			vector<LocalFile> files;
+			LocalFile file1("ibolya.txt", 8, "2020-03-04");
+			LocalFile file2("tulipan.txt", 10, "2020-03-04");
+			files.push_back(file1);
+			files.push_back(file2);
+
+			map<string, vector<LocalFile>> result = FileOps::groupFiles<string>(files, orderByDate);
 			Assert::AreEqual(1, static_cast<int>(result.size()));
 		}
 	};
